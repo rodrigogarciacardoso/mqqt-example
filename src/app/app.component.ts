29915c7028916c0119b929ad7c28e5c3e94cff89
Rodrigo Cardoso <rodrigo_cardoso97@hotmail.com>
2021-02-11 23:46:45 -0300

    inclusão mqtt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Paho } from 'ng2-mqtt/mqttws31';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mqtt-example';
+  title: any;
+  windSpeed: Number;
+  windDirection: Number;
+  private client: any;
+  mqttbroker = 'localhost';
+
+  ngOnInit(): void {
+    this.client = new Paho.MQTT.Client(this.mqttbroker, Number(9001), '283ee1c1-c8be-41f8-8d8e-940f7172b619');
+    this.client.onMessageArrived = this.onMessageArrived.bind(this);
+    this.client.onConnectionLost = this.onConnectionLost.bind(this);
+    this.client.connect({ onSuccess: this.onConnect.bind(this) });
+  }
+
+  onConnect() {
+    this.client.subscribe('exemplo/teste');
+  }
+
+  onConnectionLost(responseObject: any) {
+    if (responseObject.errorCode !== 0) {
+      console.log('onConnectionLost:' + responseObject.errorMessage);
+    }
+  }
+
+  onMessageArrived(message: any) {
+    this.title = message.payloadString
+  }
 }
